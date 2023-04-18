21a8758b054210fc4bac9591c6cc40ef01e8729a
daichangxia <changxia.dai@theone.art>
2023-04-18 23:57:24 +0800

    [新增功能](main): 0

---
+++ skuu-server/src/main/java/cn/skuu/skuuserver/SkuuServerApplication.java
@@ -0,0 +1,13 @@
+package cn.skuu.skuuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkuuServerApplication.class, args);
+    }
+
+}
---
+++ skuu-server/src/test/java/cn/skuu/skuuserver/SkuuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.skuu.skuuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
