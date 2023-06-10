5474301dfd24cb3b11d2d9cb9f80df5617413aa7
daichangxia <changxia.dai@theone.art>
2023-06-10 09:32:13 +0800

    [新增功能](main): 0

--- skuu-server/src/main/java/cn/skuu/skuuserver/SkuuServerApplication.java
+++ skuu-server/src/main/java/cn/skuu/skuuserver/SkuuServerApplication.java
@@ -3,7 +3,7 @@ package cn.skuu.skuuserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
+@SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.skuuserver", "${yudao.info.base-package}.module"})
 public class SkuuServerApplication {
 
     public static void main(String[] args) {
