c30f35cca9401db497d5b690811945b69ee3c86a
daichangxia <daichangxia>
2024-02-22 14:43:26 +0800

    新功能点: ip信息

--- src/main/java/cn/skuu/entity/IdCardHistory.java
+++ src/main/java/cn/skuu/entity/IdCardHistory.java
@@ -1,5 +1,7 @@
 package cn.skuu.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -24,6 +26,7 @@ public class IdCardHistory implements Serializable {
     /**
      * 主键
      */
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     /**
